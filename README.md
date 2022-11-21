3d6b0e0905c9085ac0a27c91eca05fdcaad84636
Anthony Farina <farinaanthony96@gmail.com>
2022-11-21 12:49:33 -0500

    v1.0.2 - Added LICENSE file; Updated requirements.txt / README.md; Refactored code; Obfuscated config file

--- README.md
+++ README.md
@@ -51,6 +51,13 @@ encounter any problems, please log an
 5. Submit a pull request ツ
 
 ## History
+-  version 1.0.2 - 2022/11/21
+    - Added LICENSE file
+    - Updated requirements.txt and README.md
+    - Refactored code
+    - Obfuscated config file
+
+
 -  version 1.0.1 - 2021/06/08
     - Added support for multiple config files
     - Updated README.md
@@ -61,26 +68,3 @@ encounter any problems, please log an
 
 ## Credits
 Anthony Farina <<farinaanthony96@gmail.com>>
-
-## License
-MIT License
-
-Copyright (c) [2021] [Anthony G. Farina]
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
--- src/PRTG-Device-IP-Changer.py
+++ src/PRTG-Device-IP-Changer.py
@@ -1,28 +1,22 @@
-import collections
-import csv
-import io
+import configparser
 import logging
 import os
-import re
 import sys
 import time
-import urllib.parse
-from datetime import datetime
+from datetime import datetime as dt
 
-import configparser
-import pandas as pd
 import pytz
 import requests
 
 
 # Module information.
 __author__ = 'Anthony Farina'
-__copyright__ = 'Copyright 2021, PRTG Device IP Changer'
+__copyright__ = 'Copyright (C) 2022 Computacenter Digital Innovation'
 __credits__ = ['Anthony Farina']
-__license__ = 'MIT'
-__version__ = '1.0.1'
 __maintainer__ = 'Anthony Farina'
 __email__ = 'farinaanthony96@gmail.com'
+__license__ = 'MIT'
+__version__ = '1.0.2'
 __status__ = 'Released'
 
 
@@ -33,102 +27,97 @@ SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 CONFIG = configparser.ConfigParser()
 CONFIG_PATH = '/../configs/PRTG-Device-IP-Changer-config.ini'
 CONFIG.read(SCRIPT_PATH + CONFIG_PATH)
-SERVER_URL = CONFIG['PRTG Info']['server-url']
-USERNAME = urllib.parse.quote_plus(CONFIG['PRTG Info']['username'])
-PASSWORD = urllib.parse.quote_plus(CONFIG['PRTG Info']['password'])
-PASSHASH = urllib.parse.quote_plus(CONFIG['PRTG Info']['passhash'])
+
+# PRTG global variables.
+PRTG_INSTANCE_URL = CONFIG['PRTG Info']['server-url']
+PRTG_API_TABLE = '/api/table.xml'
+PRTG_FETCH_URL = PRTG_INSTANCE_URL + PRTG_API_TABLE
+PRTG_API_CHANGE_PROPERTY = '/api/setobjectproperty.htm'
+PRTG_CHANGE_PROPERTY_URL = PRTG_INSTANCE_URL + PRTG_API_CHANGE_PROPERTY
+PRTG_USERNAME = CONFIG['PRTG Info']['username']
+PRTG_PASSWORD = CONFIG['PRTG Info']['password']
+
+# Other global variables from the config file.
 TIMEZONE = CONFIG['Timezone Info']['timezone']
-CSV_FILE = CONFIG['CSV Info']['file-name']
 
 
-# This method will go through the provided CSV file of devices and their new
-# IP addresses. Then it will edit each listed device to their new IP address
-# listed in the provided CSV file. It will log everything in a log file.
-def prtg_device_ip_changer() -> None:
-    # Make a logger that logs what's happening in a log file and the console.
-    now_log = datetime.utcnow().replace(tzinfo=pytz.UTC).astimezone(
+# Given a dictionary of devices and their desired IPv4 address
+# changes, change those device's IPv4 addresses in PRTG.
+def prtg_device_ip_changer(prtg_device_dict: dict) -> None:
+    # Make a logger that logs to a file and the console.
+    now_dt = dt.utcnow().replace(tzinfo=pytz.UTC).astimezone(
         pytz.timezone(TIMEZONE))
-    logging.basicConfig(filename=SCRIPT_PATH + '/../logs/ip_changer_log-' +
-                        now_log.strftime('%Y-%m-%d_%I-%M-%S-%p-%Z') + '.log',
+    logging.basicConfig(filename=(
+                            SCRIPT_PATH +
+                            '/../logs/prtg_device_ip_changer_log_' +
+                            now_dt.strftime('%Y-%m-%d_%I-%M-%S-%p-%Z') +
+                            '.log'),
                         level=logging.INFO,
                         format='[%(asctime)s] [%(levelname)s] %(message)s',
                         datefmt='%m-%d-%Y %I:%M:%S %p %Z')
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
-    # Prepare the URL to get all device information from PRTG.
-    device_url = SERVER_URL + \
-                 '/api/table.xml?content=devices&columns=' \
-                 'name,objid,group' \
-                 '&output=csvtable&count=50000' \
-                 '&username=' + USERNAME
-    device_url = add_auth(device_url)
+    # Get the device information from PRTG and convert it to JSON.
+    logging.info('Retrieving devices from PRTG...')
+    prtg_devices_resp = requests.get(url=PRTG_FETCH_URL,
+                                     params={
+                                        'content': 'devices',
+                                        'columns': 'group,name,objid,host',
+                                        'filter_name': list(
+                                            prtg_device_dict.keys()),
+                                        'count': '50000',
+                                        'output': 'json',
+                                        'username': PRTG_USERNAME,
+                                        'password': PRTG_PASSWORD
+                                     })
+    prtg_devices = prtg_devices_resp.json()
+    logging.info('Devices retrieved from PRTG!')
 
-    # Get the device information from PRTG.
-    logging.info('Retrieving all devices from PRTG...')
-    device_resp = requests.get(url=device_url)
-    logging.info('All devices retrieved from PRTG!')
-
-    # Make a clean dataframe object from the device information received from
-    # PRTG.
-    logging.info('Formatting response from PRTG...')
-    device_resp_csv_strio = io.StringIO(device_resp.text)
-    device_csv_reader = csv.reader(device_resp_csv_strio,
-                                   delimiter=',')
-
-    # Make the device ID dictionary.
-    # device[0] is the device's name (the key)
-    # device[2] is the device's object ID (the value)
-    device_id_dict = collections.defaultdict(str)
-
-    for device in device_csv_reader:
-        device_id_dict[device[0]] = device[2]
-
-    # Load provided CSV of devices and their new IP addresses.
-    new_ip_csv_reader = csv.reader(open(CSV_FILE))
-    next(new_ip_csv_reader)
-    logging.info('Response from PRTG has been formatted!')
-
-    # Count the number of successful and unsuccessful edits along with the
-    # connection reuse object.
+    # Count the number of successful and unsuccessful edits along with
+    # the connection session object.
     edits = 0
     errors = 0
     edit_sess = requests.Session()
 
-    # Iterate through all devices and edit specified devices by name.
-    for device in new_ip_csv_reader:
-        # Log that we are about to make a change to a device.
-        logging.info('Editing the IP address of device \"' +
-                     device[0] + '\" (ID: ' +
-                     device_id_dict[device[0]] + ') from PRTG...')
-
-        # Prepare the edit URL.
-        edit_ip_url = SERVER_URL + '/api/setobjectproperty.htm?id=' + \
-                      device_id_dict[device[0]] + \
-                      '&name=host&value=' + device[1] + \
-                      '&username=' + USERNAME
-        edit_ip_url = add_auth(edit_ip_url)
+    # Iterate through the devices and edit their IP addresses.
+    for prtg_device in prtg_devices['devices']:
+        # Log the IP change for this device.
+        logging.info('Editing the IP address of device ' +
+                     prtg_device['group'] + ' > ' + prtg_device['name'] +
+                     ' from + ' + prtg_device['host'] + ' to ' +
+                     prtg_device_dict[prtg_device['name']] + '...')
 
         # Send the edit request to PRTG.
-        time.sleep(10)
-        edit_ip_resp = edit_sess.get(url=edit_ip_url)
+        time.sleep(2)
+        edit_ip_resp = edit_sess.get(url=PRTG_CHANGE_PROPERTY_URL,
+                                     params={
+                                         'id': prtg_device['objid'],
+                                         'name': 'host',
+                                         'value':
+                                             prtg_device_dict[
+                                                 prtg_device['name']],
+                                         'username': PRTG_USERNAME,
+                                         'password': PRTG_PASSWORD
+                                     })
 
-        # Check if the IP address edit was successful.
+        # Check if the edit was unsuccessful.
         if edit_ip_resp.status_code != 200:
-            logging.error('Error changing the IP address of device \"'
-                          + device[0] + '\" (ID: '
-                          + device_id_dict[device[0]] + ' in PRTG.')
+            logging.error('Error changing the IP address of device ' +
+                          prtg_device['group'] + ' > ' + prtg_device['name'] +
+                          ' to ' + prtg_device_dict[prtg_device['name']] +
+                          ' in PRTG.')
             logging.error('Caused by: ' + str(edit_ip_resp.status_code))
             logging.error(edit_ip_resp.reason)
             errors += 1
         # The IP address edit was successful.
         else:
-            logging.info('Device \"' + device[0] + '\" (ID: ' +
-                         device_id_dict[device[0]] +
-                         ') IP address successfully changed to ' +
-                         device[1] + '!')
+            logging.info('Device ' + prtg_device['group'] + ' > ' +
+                         prtg_device['name'] +
+                         ' IP address successfully changed to ' +
+                         prtg_device_dict[prtg_device['name']] + '!')
             edits += 1
 
-    # Close the connection to PRTG.
+    # Close the connection session to PRTG.
     edit_sess.close()
 
     # Log the results and end the script.
@@ -140,47 +129,19 @@ def prtg_device_ip_changer() -> None:
     logging.info('Total device IP address edit errors: ' + str(errors))
 
 
-# Every time table information is called from the PRTG API, the response has
-# 'readable' columns and 'raw' columns. Their are subtle differences,
-# but the raw columns are not needed. This function removes all the 'raw'
-# columns from a dataframe object of the PRTG API response and returns a
-# dataframe object with only the non-raw columns.
-def remove_raw(raw_df: pd.DataFrame) -> pd.DataFrame:
-    # Prepare a list of desired column names.
-    col_labels = list()
-
-    # Iterate through the column labels to remove column labels ending with
-    # '(RAW)'.
-    for col in raw_df.columns:
-        # Add only desired column labels to the list.
-        if not bool(re.search('\\(RAW\\)$', col)):
-            col_labels.append(col)
-
-    # Return the dataframe object that only has desired columns.
-    return_df = raw_df[col_labels]
-    return return_df
-
-
-# This function will append the PRTG authentication to the end of the given
-# PRTG API call URL. It will append either the password or passhash,
-# whichever was provided in the config file. Passhash has priority if both
-# fields are filled in.
-def add_auth(url: str) -> str:
-    # Check if the password or passhash will be used to authenticate the
-    # access to the PRTG instance.
-    if PASSHASH == '':
-        url = url + '&password=' + PASSWORD
-    else:
-        url = url + '&passhash=' + PASSHASH
-
-    return url
-
-
 # The main method that runs the script. There are no input arguments.
 if __name__ == '__main__':
-    # Check to make sure the logs folder exists. If not, create it.
+    # Check to make sure the "logs" folder exists. If not, create it.
     if not os.path.isdir(SCRIPT_PATH + '/../logs'):
         os.mkdir(SCRIPT_PATH + '/../logs')
 
+    # Test dictionary that will be passed to the script.
+    devices = {
+        'device1': '192.168.0.1',
+        'device2': '192.168.0.2'
+    }
+
+    # TODO - Add the functionality of this script to the PRTG-FastAPI
+    #        project?
     # Run the script.
-    prtg_device_ip_changer()
+    prtg_device_ip_changer(devices)
